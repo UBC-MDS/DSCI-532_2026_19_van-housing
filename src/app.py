5bf77f212ee4b02c29cf107be917d369bcd79b75
Claudia <87799252+claudia-liauw@users.noreply.github.com>
2026-02-27 14:43:31 -0800

    data wrangling

--- src/app.py
+++ src/app.py
@@ -1,4 +1,23 @@
-from shiny import App, ui
+from shiny import App, ui, reactive
+import pandas as pd
+
+data = pd.read_csv('../data/raw/non-market-housing.csv', sep=';')
+
+# Data wrangling
+data.rename(columns={'Clientele- Families': 'Clientele - Families'}, inplace=True)
+data = data.loc[data['Project Status'] == 'Completed']
+
+data['Clientele'] = 'Mixed'
+data.loc[(data['Clientele - Seniors'] == 0) & (data['Clientele - Other'] == 0), 'Clientele'] = 'Families'
+data.loc[(data['Clientele - Families'] == 0) & (data['Clientele - Other'] == 0), 'Clientele'] = 'Seniors'
+
+room_types = ['1BR', '2BR', '3BR', '4BR', 'Studio']
+for br in room_types:
+    data[f'{br} Available'] = (data.filter(like=br).sum(axis=1) > 0).astype('int')
+
+access_types = ['Accessible', 'Adaptable', 'Standard']
+for ac in access_types:
+    data[f'{ac} Available'] = (data.filter(like=ac).sum(axis=1) > 0).astype('int')
 
 app_ui = ui.page_fillable(
     ui.panel_title("Title"),
@@ -18,7 +37,9 @@ app_ui = ui.page_fillable(
 
 
 def server(input, output, session):
-    pass
+    @reactive.calc
+    def df():
+        return data
 
 
 app = App(app_ui, server=server)
