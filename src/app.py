5cd580090890d775bfb0d6273146db48753247a6
William Chong <will.hh.chong@gmail.com>
2026-03-06 18:13:49 -0800

    Merge fixed, filter conflict on clientele fixed

--- src/app.py
+++ src/app.py
@@ -145,7 +145,6 @@ app_ui = ui.page_fillable(
             overflow-y: auto;
         }
     """),
-<<<<<<< HEAD
 
     ui.h2(
         "Non-market Housing Dashboard for the City of Vancouver",
@@ -264,42 +263,6 @@ app_ui = ui.page_fillable(
                         """
                     )
                 )
-=======
-    ui.h2("Non-market Housing Dashboard for the City of Vancouver", style="text-align:center; font-weight:700; font-size: 40px"),
-    ui.p("Below are the buildings that match your selections.", style="text-align:center; margin-top:-8px; font-size: 24px; color:#666;"),
-    ui.page_sidebar(
-        ui.sidebar(
-            ui.h4("Filters"),
-            ui.input_checkbox_group(
-                "clientele",
-                "Clientele",
-                ["Families", "Seniors", "Mixed"]
-            ),
-            ui.input_selectize(
-                "br",
-                "Bedrooms",
-                ["1BR", "2BR", "3BR", "4BR"],
-                multiple=True
-            ),
-            ui.input_selectize(
-                "accessible",
-                "Accessibility",
-                ["Standard", "Adaptable", "Accessible"],
-                multiple=True
-            ),
-            ui.input_slider(
-                "year",
-                "Year",
-                min=date(1971, 1, 1), max=date(2025, 12, 31),
-                value=[date(1971, 1, 1), date(2025, 12, 31)],
-                time_format='%Y'
-            ),
-            ui.input_action_button(
-                "reset",
-                "Reset Filters",
-                class_="btn btn-secondary",
-                style="margin-top: 15px; width: 100%;"
->>>>>>> origin/dev
             )
         ),
 
@@ -350,7 +313,7 @@ def server(input, output, session):
 
         if input.clientele():
             filtered_data = filtered_data[
-                filtered_data.Clientele.isin(input.clientele())
+                filtered_data["Clientele"] == (input.clientele())
             ]
 
         if input.br():
